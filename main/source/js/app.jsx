f2f41facbd96e9c08b0fa897c2c227980c13337a
BeeNag <bnaglerose88@gmail.com>
2016-01-02 12:21:25 +0000

    Update to code architecture

--- main/source/js/app.jsx
+++ main/source/js/app.jsx
@@ -1,13 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ArchLandingPage = require('./components/arch-landing-page/ArchLandingPage.jsx');
-var ArchaeologistProfile = require('./components/archaeologist-profile/ArchaeologistProfile.jsx');
-var CompanyProfile = require('./components/company-profile/CompanyProfile.jsx');
-var CreateCompanyProfile = require('./components/create-company-profile/CreateCompanyProfile.jsx');
-var CreateExcavation = require('./components/create-excavation/CreateExcavation.jsx');
-var CreateProfile = require('./components/create-profile/CreateProfile.jsx');
-var EmployerLandingPage = require('./components/employer-landing-page/EmployerLandingPage.jsx');
-var LandingPage = require('./components/landing-page/LandingPage.jsx');
-var Search = require('./components/search/Search.jsx');
+var Application = require('./components/Application.jsx');
 
-ReactDOM.render(<LandingPage />, document.querySelector('[data-react-application]'));
+ReactDOM.render(<Application />, document.querySelector('[data-react-application]'));
---
+++ main/source/js/components/Application.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ArchLandingPage = require('./components/arch-landing-page/ArchLandingPage.jsx');
+var ArchaeologistProfile = require('./components/archaeologist-profile/ArchaeologistProfile.jsx');
+var CompanyProfile = require('./components/company-profile/CompanyProfile.jsx');
+var CreateCompanyProfile = require('./components/create-company-profile/CreateCompanyProfile.jsx');
+var CreateExcavation = require('./components/create-excavation/CreateExcavation.jsx');
+var CreateProfile = require('./components/create-profile/CreateProfile.jsx');
+var EmployerLandingPage = require('./components/employer-landing-page/EmployerLandingPage.jsx');
+var LandingPage = require('./components/landing-page/LandingPage.jsx');
+var Search = require('./components/search/Search.jsx');
+
+var Application = React.createClass({
+	render: function () {
+		return (
+			<LandingPage />
+		);
+	}
+});
