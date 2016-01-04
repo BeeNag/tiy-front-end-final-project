c657344b5ddfada23d04897eddb2a4d54d522120
BeeNag <bnaglerose88@gmail.com>
2016-01-04 12:33:47 +0000

    Minor update

--- main/source/js/components/landing-page/ArchaeologistProfileDetails.jsx
+++ main/source/js/components/landing-page/ArchaeologistProfileDetails.jsx
@@ -12,6 +12,8 @@ var ArchaeologistProfileDetails = React.createClass({
 				<input type="address" placeholder="Address Line 3" className="form-control input pass"></input>
 				<input type="text" placeholder="City" className="form-control input pass"></input>
 				<input type="postcode" placeholder="Postcode" className="form-control input pass"></input>
+				<input type="text" placeholder="Enter Home Phone Number" className="form-control input pass" data-format="(+44)ddd ddd dddd"></input>
+				<input type="text" placeholder="Enter Mobile Phone Number" className="form-control input pass" data-format="(+44)dddd ddd dddd"></input>
 				<select className="form-control input pass">
             		<option>Experience</option>
             		<option>0-1</option>
--- main/source/js/components/landing-page/CompanyProfileDetails.jsx
+++ main/source/js/components/landing-page/CompanyProfileDetails.jsx
@@ -10,6 +10,7 @@ var CompanyProfileDetails = React.createClass({
 				<input type="address" placeholder="Address Line 3" className="form-control input pass"></input>
 				<input type="text" placeholder="City" className="form-control input pass"></input>
 				<input type="postcode" placeholder="Postcode" className="form-control input pass"></input>
+				<input type="text" placeholder="Enter Phone Number" className="form-control input pass" data-format="(+44)ddd ddd dddd"></input>
 				<input type="url" placeholder="Company Home Page URL" className="form-control input pass"></input>
 				<textarea className="form-control input pass" placeholder="Please Enter a Brief Description of the Company (max. 250 words)" rows="5" maxlength="250"></textarea>
 			</div>
