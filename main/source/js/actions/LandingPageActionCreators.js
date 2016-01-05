29200ae95fe8fea80c571798a6d46828494e93a5
BeeNag <bnaglerose88@gmail.com>
2016-01-05 09:36:28 +0000

    LandingPageActionCreator update

---
+++ main/source/js/actions/LandingPageActionCreators.js
@@ -0,0 +1,23 @@
+var Dispatcher = require('../dispatcher/Dispatcher.js');
+
+function changeToArchLandingPage() {
+	var action = {
+		type: 'change-to-arch-landing-page'
+	};
+
+	Dispatcher.dispatch(action);
+}
+
+function changeToEmployerLandingPage() {
+	var action = {
+		type: 'change-to-employer-landing-page'
+	};
+
+	Dispatcher.dispatch(action);
+}
+
+module.exports = {
+	changeToArchLandingPage: changeToArchLandingPage,
+	changeToEmployerLandingPage: changeToEmployerLandingPage
+};
+
