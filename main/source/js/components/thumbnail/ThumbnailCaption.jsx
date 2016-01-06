a4f28903f652e011287bd2fbe7a0f8f8b27460a1
BeeNag <bnaglerose88@gmail.com>
2016-01-06 16:44:25 +0000

    Minor update

--- main/source/js/components/thumbnail/ThumbnailCaption.jsx
+++ main/source/js/components/thumbnail/ThumbnailCaption.jsx
@@ -4,11 +4,11 @@ var ThumbnailCaption = React.createClass({
 	render: function () {
 		return (
 			<div className="caption">
-				<h4>{this.props.name}</h4>
-				<p>{this.props.email}</p>
-				<p>{this.props.specialism}</p>
-				<p>{this.props.experience}</p>
-				<p><a href="#" className="btn btn-primary" role="button">View Profile</a></p>
+				<h4 className="text-center">{this.props.name}</h4>
+				<p className="text-center">{this.props.email}</p>
+				<p className="text-center">{this.props.specialism}</p>
+				<p className="text-center">{this.props.experience}</p>
+				<p className="text-center"><a href="#" className="btn btn-primary" role="button">View Profile</a></p>
 			</div>
 		);
 	}
