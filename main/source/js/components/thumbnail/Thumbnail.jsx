d872e92f315802828b8e8f86367b6618a5aaf229
BeeNag <bnaglerose88@gmail.com>
2016-01-06 17:32:20 +0000

    Thumb now gets data from store

--- main/source/js/components/thumbnail/Thumbnail.jsx
+++ main/source/js/components/thumbnail/Thumbnail.jsx
@@ -1,14 +1,15 @@
 var React = require('react');
 var ThumbnailImage = require('./ThumbnailImage.jsx');
 var ThumbnailCaption = require('./ThumbnailCaption.jsx');
+var SearchStore = require('../../stores/SearchStore.js');
 
 var Thumbnail = React.createClass({
 	render: function () {
 		return (
 			<div className="col-xs-3">
 				<div className="thumbnail">
-					<ThumbnailImage imageUrl="http://www.valuestockphoto.com/downloads/43521-2/power_button.jpg" imageAlt="power switched off" />
-					<ThumbnailCaption name="Joe Blogs" email="joeblogs1988@gmail.com" specialism="Osteoarchaeology" experience="2-5 Years" />
+					<ThumbnailImage imageUrl={SearchStore.getThumbnailData().imageUrl} imageAlt={SearchStore.getThumbnailData().imageAlt} />
+					<ThumbnailCaption name={SearchStore.getThumbnailData().name} email={SearchStore.getThumbnailData().email} specialism={SearchStore.getThumbnailData().specialism} experience={SearchStore.getThumbnailData().experience} />
 				</div>
 			</div>
 		);
--- main/source/js/stores/SearchStore.js
+++ main/source/js/stores/SearchStore.js
@@ -1,3 +1,29 @@
 var Dispatcher = require('../dispatcher/Dispatcher.js');
 var EventEmitter = require('events').EventEmitter;
 var objectAssign = require('object-assign');
+
+var thumbnailData = {
+	imageUrl: "http://www.valuestockphoto.com/downloads/43521-2/power_button.jpg",
+	imageAlt: "power switched off",
+	name: "Joe Bloggs",
+	email: "joebloggs@gmail.com",
+	specialism: "Osteoarchaeology",
+	experience: "2-5 Years"
+};
+
+var SearchStore = objectAssign({}, EventEmitter.prototype, {
+
+	getThumbnailData: function () {
+		return thumbnailData;
+	},
+
+	addChangeListener: function (changeEventHandler) {
+		this.on('change', changeEventHandler);
+	},
+
+	removeChangeListener: function (changeEventHandler) {
+		this.removeListener('change', changeEventHandler);
+	}
+});
+
+module.exports = SearchStore;
