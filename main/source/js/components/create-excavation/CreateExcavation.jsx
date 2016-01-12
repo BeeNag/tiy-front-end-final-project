7f8c8f480b2895ad1ae8d4da23a74617bf6dd8a5
BeeNag <bnaglerose88@gmail.com>
2016-01-12 14:42:35 +0000

    Add refs to create excavation form

--- main/source/js/components/create-excavation/CreateExcavation.jsx
+++ main/source/js/components/create-excavation/CreateExcavation.jsx
@@ -18,48 +18,39 @@ var CreateExcavation = React.createClass({
 					<form>
 						<div className="form-group">
 							<label htmlFor="inputExcavationName">Excavation Name</label>
-							<input type="text" className="form-control" id="inputExcavationName" placeholder="Excavation Name"></input>
+							<input type="text" className="form-control" id="inputExcavationName" placeholder="Excavation Name" ref="name"></input>
 						</div>
 						<div className="form-group">
 	                    	<label htmlFor="inputAddressLineOne">Address Line 1</label>
-	                    	<input type="address" className="form-control" id="inputAddressLineOne" placeholder="Address Line 1"></input>
+	                    	<input type="address" className="form-control" id="inputAddressLineOne" placeholder="Address Line 1" ref="address1"></input>
 	                    </div>
 	                    <div className="form-group">
 	                    	<label htmlFor="inputAddressLineTwo">Address Line 2</label>
-	                    	<input type="address" className="form-control" id="inputAddressLineTwo" placeholder="Address Line 2"></input>
+	                    	<input type="address" className="form-control" id="inputAddressLineTwo" placeholder="Address Line 2" ref="address2"></input>
 	                    </div>
 	                    <div className="form-group">
 	                    	<label htmlFor="inputAddressLineThree">Address Line 3</label>
-	                    	<input type="address" className="form-control" id="inputAddressLineThree" placeholder="Address Line 3"></input>
+	                    	<input type="address" className="form-control" id="inputAddressLineThree" placeholder="Address Line 3" ref="address3"></input>
 	                    </div>
 	                    <div className="form-group">
 	                    	<label htmlFor="inputPostcode">Postcode</label>
-	                    	<input type="postcode" className="form-control" id="inputPostcode" placeholder="Postcode"></input>
+	                    	<input type="postcode" className="form-control" id="inputPostcode" placeholder="Postcode" ref="postcode"></input>
 	                    </div>
 	                    <div className="form-group">
 	                    	<label htmlFor="selectDuration">Duration of Excavation</label>
-	                    	<select className="form-control" id="selectDuration">
-	                    		<option></option>
-	                    		<option>1 Week</option>
-	                    		<option>2 Weeks</option>
-	                    		<option>3 Weeks</option>
-	                    		<option>1 Month</option>
-	                    		<option>2 Months</option>
-	                    		<option>3 Months</option>
-	                    		<option>More than 3 Months</option>
-	                    	</select>
+	                    	<input type="text" className="form-control" id="selectDuration" placeholder="Duration" ref="duration"></input>
 	                    </div>
 	                    <div className="form-group">
       						<label htmlFor="enterExcavationUrl">Excavation Home Page</label>
-      						<input type="url" className="form-control" id="enterExcavationUrl" placeholder="Excavation Home Page URL"></input>
+      						<input type="url" className="form-control" id="enterExcavationUrl" placeholder="Excavation Home Page URL" ref="url"></input>
     					</div>
     					<div className="form-group">
 	                    	<label htmlFor="describeExcavation">Please Enter a Brief Description of the Excavation (max. 250 words)</label>
-	                    	<textarea className="form-control" id="describeExcavation" rows="5" maxlength="250"></textarea>
+	                    	<input type="text" className="form-control" id="describeExcavation" placeholder="Description" ref="description"></input>
 	                    </div>
 					</form>
 				</div>
-				<div className="row">
+				<div className="form-group">
 					<div className="col-xs-6 col-xs-offset-3">
 						<button onClick={this.handleCreateExcavationClickEvent} type="button" className="btn btn-success">Create Excavation Profile</button>
 					</div>
